5accd10511fb697cd8370464f3b54ac7eb0eb8cf
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2020-08-06 17:59:16 -0400

    feat: pages and static queries

---
+++ tutorial/src/pages/examples.js
@@ -0,0 +1,41 @@
+import React from "react"
+import Layout from "../components/Layout"
+import HeaderStatic from "../examples/HeaderStatic"
+import Header from "../examples/Header"
+import { graphql } from "gatsby"
+
+const examples = ({ data }) => {
+  const {
+    site: {
+      info: { author },
+    },
+  } = data
+  return (
+    <Layout>
+      <div>
+        <h1>Hello from examples page!</h1>
+        <Header />
+        <HeaderStatic />
+        <h5>Author: {author}</h5>
+      </div>
+    </Layout>
+  )
+}
+
+export const data = graphql`
+  query MyQuery {
+    site {
+      info: siteMetadata {
+        title
+        description
+        author
+        data
+        person {
+          name
+          age
+        }
+      }
+    }
+  }
+`
+export default examples
