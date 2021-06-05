4dfedcd545a3295cc91426ba9dd99497eede6e20
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2021-06-04 23:04:28 -0300

    feat: mdx project almost done

--- mdx-blog-main/src/templates/category-template.js
+++ mdx-blog-main/src/templates/category-template.js
@@ -1,17 +1,48 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Hero from '../components/Hero'
-import Posts from '../components/Posts'
-import { graphql } from 'gatsby'
+import React from "react";
+import Layout from "../components/Layout";
+import Hero from "../components/Hero";
+import Posts from "../components/Posts";
+import {graphql} from "gatsby";
 
-const CategoryTemplate = ()=> {
-  
+const CategoryTemplate = ({
+  data: {
+    allMdx: {nodes: posts},
+  },
+  pageContext: {category},
+}) => {
   return (
-   <h2>category template</h2>
-  )
-}
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title={`category / ${category}`} />
+    </Layout>
+  );
+};
 
+export const query = graphql`
+  query getCategories($category: String) {
+    allMdx(
+      sort: {fields: frontmatter___date, order: DESC}
+      filter: {frontmatter: {category: {eq: $category}}}
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`;
 
-
-
-export default CategoryTemplate
+export default CategoryTemplate;
